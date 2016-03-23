431e631daa7525ed11392fcea00a7aa4b6ad73ed
MaxKostyniuk <maxeem23@gmail.com>
2016-03-23 13:34:35 +0200

    array readline

--- academy/academy/Program.cs
+++ academy/academy/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace academy
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Library book = new Library();
+            book.Input();
+            /*Book my}Book;
+            myBook.title = "Clicker";
+            myBook.author = "Alan Wake";
+            myBook.Display();
+            Book b2 = new Book("monster", "Mary Shelley");
+            Console.ReadLine();
+             * */
         }
     }
+    struct Book
+    {
+        public string title;
+    }
+    public class Library
+    {
+        
+        Book[] bookArr = new Book[4];
+        public void Input()
+        {
+            
+            //string[] booksArray = new string[5];
+            for (int i = 0; i < 4; i++)
+            {
+
+                bookArr[i] = ToString(Console.ReadLine());
+                
+            }
+        }
+    }
+   
 }
